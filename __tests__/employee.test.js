db75e56fd6088f860b870462d7a0bf4a8ce83611
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-31 15:04:05 -0500

    corrected test code and made it pass

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,23 +1,33 @@
-employee = require('../employee.js');
+const Employee = require('../lib/employee');
 
 
 
 
 test("Checking Properties", () => {
-
+        const employee = new Employee('Dave', '3', 'dave@gmail.com');
         expect(employee).toHaveProperty('name');
 
         expect(employee).toHaveProperty('id');
 
         expect(employee).toHaveProperty('email');
 
-        expect(employee.getName()).toBe(expect(any(string)))
+        expect(employee.getName()).toEqual(expect.any(String))
 
-        expect(employee.getEmail()).toBe(expect(any(string)))
+        expect(employee.getEmail()).toEqual(expect.any(String))
 
-        expect(employee.getID()).toBe(expect(any(string)))
+        expect(employee.getID()).toEqual(expect.any(String))
 
-        expect(employee.getRole()).toBe(expect(any(string)))
+        expect(employee.getRole()).toEqual(expect.any(String))
+
+
+        expect(employee.getRole()).toBe('Employee')
+
+        expect(employee.getEmail()).toBe('dave@gmail.com')
+
+        expect(employee.getID()).toBe('3')
+
+        expect(employee.getName()).toBe('Dave')
+
 
     }
 
