ce320ac79ed7e9fb89b8765bd4357431e16430b2
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-31 20:49:27 -0500

    wrote failing test for intern

---
+++ __tests__/intern.test.js
@@ -0,0 +1,36 @@
+const Intern = require('../lib/intern');
+
+
+
+test("Checking Properties of Intern and instantiating new object and testing it", () => {
+    const intern = new Intern('Dave', '3', 'dave@gmail.com', 'Eastern Heights');
+    expect(intern).toHaveProperty('name');
+
+    expect(intern).toHaveProperty('school');
+
+    expect(intern).toHaveProperty('id');
+
+    expect(intern).toHaveProperty('email');
+
+
+    expect(intern.getName()).toEqual(expect.any(String))
+
+    expect(intern.getEmail()).toEqual(expect.any(String))
+
+    expect(intern.getID()).toEqual(expect.any(String))
+
+    expect(intern.getRole()).toEqual(expect.any(String))
+
+    expect(intern.getRole()).toBe('intern')
+
+
+
+    expect(intern.getEmail()).toBe('dave@gmail.com')
+
+    expect(intern.getID()).toBe('3')
+
+    expect(intern.getName()).toBe('Dave')
+
+    expect(intern.getSchool()).toBe('Eastern Heights')
+
+})
